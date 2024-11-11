c6a49fa0259536dbe61285439ef5de60eabec918
Monsur Raji <monsurrajiybs@gmail.com>
2024-11-11 17:41:23 +0100

    ss

---
+++ app/dashboard/payments/page.tsx
@@ -0,0 +1,12 @@
+// app/dashboard/payments/page.tsx
+import React from 'react';
+
+const PaymentsPage = () => {
+  return (
+    <div>
+      <h1>Payments</h1>
+    </div>
+  );
+};
+
+export default PaymentsPage;
---
+++ app/dashboard/transactions/page.tsx
@@ -0,0 +1,12 @@
+// app/dashboard/payments/page.tsx
+import React from 'react';
+
+const transactions = () => {
+  return (
+    <div>
+      <h1>Payments</h1>
+    </div>
+  );
+};
+
+export default transactions;
