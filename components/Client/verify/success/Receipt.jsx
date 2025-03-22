5c271d2710d1cda26a193e4350a7536e397fc6a0
Monsur Raji <monsurrajiybs@gmail.com>
2025-03-22 12:22:37 +0100

    ss

--- components/Client/verify/success/Receipt.jsx
+++ components/Client/verify/success/Receipt.jsx
@@ -101,11 +101,13 @@
             const isTransactionId = detail.label === 'Transaction ID';
             const isTotalAmount = detail.label === 'Total Amount';
             const displayValue = isTransactionId
-            ? fullTransactionId || (isSaving ? detail.value : detail.value.slice(0, 8) + '...') // Truncate normally
-            : isTotalAmount
-            ? `₦${detail.value}`
-            : detail.value;
-          
+              ? fullTransactionId || isSaving
+                ? detail.value
+                : detail.value.slice(0, 8) + '...' // Truncate normally
+              : isTotalAmount
+              ? `₦${detail.value}`
+              : detail.value;
+              console.log(displayValue);
 
             return (
               <div key={index} className="flex justify-between items-center mt-2">
