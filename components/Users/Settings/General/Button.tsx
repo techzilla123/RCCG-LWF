8d93bc02dcdf8779ec7eb04f1dee600417fda3cd
Monsur Raji <monsurrajiybs@gmail.com>
2025-05-12 14:19:28 +0100

    ss

--- components/Users/Settings/General/Button.tsx
+++ components/Users/Settings/General/Button.tsx
@@ -5,12 +5,14 @@ interface ButtonProps {
   variant?: "primary" | "secondary";
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string; // optional external className
 }
 
 export const Button: React.FC<ButtonProps> = ({
   variant = "primary",
   children,
   onClick,
+  className = "",
 }) => {
   const baseStyles =
     "flex items-center justify-center gap-2 self-stretch p-4 h-10 font-medium text-base tracking-normal leading-6 text-center min-h-10 rounded-[50px]";
@@ -20,7 +22,10 @@ export const Button: React.FC<ButtonProps> = ({
       : "text-black bg-black bg-opacity-0";
 
   return (
-    <button className={`${baseStyles} ${variantStyles}`} onClick={onClick}>
+    <button
+      className={`${baseStyles} ${variantStyles} ${className}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
