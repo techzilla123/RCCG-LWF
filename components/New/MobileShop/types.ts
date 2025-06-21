e69f22bba94d9ebdbefa6eb8b54e3b3696357444
Monsur Raji <monsurrajiybs@gmail.com>
2025-06-21 11:09:24 +0100

    ss

--- components/New/MobileShop/types.ts
+++ components/New/MobileShop/types.ts
@@ -1,9 +1,9 @@
 export interface Product {
-    id: string;
-    image: string;
-    title: string;
-    price: number;
-    isOutOfStock?: boolean;
-    isWishlisted?: boolean;
-  }
-  
+  id: string;
+  image: string;
+  title: string;
+  price: number;
+  isOutOfStock?: boolean;
+  isWishlisted?: boolean;
+  isAdded?: boolean; // if you're tracking cart add state
+}
