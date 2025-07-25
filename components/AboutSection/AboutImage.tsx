ca5f52f2ad9d5b0b5a1c4f69a0af66939d2c4e15
Monsur Raji <monsurrajiybs@gmail.com>
2025-07-25 17:45:05 +0100

    ss

--- components/AboutSection/AboutImage.tsx
+++ components/AboutSection/AboutImage.tsx
@@ -5,7 +5,7 @@ export function AboutImage() {
     <img
       src="/0O0A7266.JPG"
       alt="About section illustration"
-      className="object-contain -ml-6 self-stretch my-auto aspect-[1.13] min-w-60 w-[532px] max-md:max-w-full"
+      className="object-contain self-center my-auto aspect-[1.13] min-w-[280px] w-[600px] max-w-full transition-transform duration-300 hover:scale-105 shadow-2xl rounded-2xl glow"
     />
   );
 }
