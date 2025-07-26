dd5123f2f350087c3308c008dd3cf166315c0e5e
Monsur Raji <monsurrajiybs@gmail.com>
2025-07-26 11:33:51 +0100

    ss

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -42,7 +42,7 @@ const Offers: React.FC = () => {
 
   <div className="flex flex-wrap justify-center items-center pt-6 w-full max-w-[1400px] max-md:max-w-full">
     <OfferImages
-      firstImageUrl="https://cdn.builder.io/api/v1/image/assets/8508077b32c64a2d81a17cc6a85ba436/0c4b41de8c8c252dbff2a164b2f9552456f29dc4?placeholderIfAbsent=true"
+      firstImageUrl="/untitled.svg"
       secondImageUrl="https://cdn.builder.io/api/v1/image/assets/8508077b32c64a2d81a17cc6a85ba436/4828825885b07851dceda276ef48021429ee70e6?placeholderIfAbsent=true"
     />
     <ActionButton onClick={handleSignUp}>Sign up now</ActionButton>
