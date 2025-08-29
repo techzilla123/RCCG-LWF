c22d9953cc499f35c13f2bfdde306dfd53f87495
Monsur Raji <monsurrajiybs@gmail.com>
2025-08-29 15:35:40 +0100

    ss

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-// import Hero from "@/components/Hero";
+
 import { usePathname } from 'next/navigation';
 import Offer from "@/components/Offer";
 import TopNavBar from "@/components/TopNavBar";
@@ -7,37 +7,42 @@ import Header from "@/components/Users/Rentals/Header"
 import { Shop } from "@/components/Users/Shop"
 import { Suspense } from "react";
 import BotpressChat from "@/components/BotpressChat"; 
- 
-
 import Footer from "@/components/Footer"
-
-// import Footer from "@/components/Footer";
-
+import Script from "next/script";
 
 export default function Home() {
 
-   const pathname = usePathname();
-
+  const pathname = usePathname();
   const shouldShowBotpress = pathname === '/rentals'; 
 
   return (
-    <div className="min-h-scren bg-white">
+    <div className="min-h-screen bg-white">
+      {/* Google Tag (gtag.js) */}
+      <Script 
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17512936136" 
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17512936136');
+        `}
+      </Script>
+
       <Offer />
       <TopNavBar/>
-       <Suspense fallback={<div>Loading...</div>}>
-      <Header/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header/>
       </Suspense>
-    <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <Shop />
       </Suspense>
-      
+
       {shouldShowBotpress && <BotpressChat />}
 
-      <Footer/>
-
-
-      {/* <Footer />
-     */}
+      <Footer />
     </div>
   );
 }
