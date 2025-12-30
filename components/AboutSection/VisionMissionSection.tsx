f8ddbd9c1a8ec4a886e883d5770ac371f5d1a367
Monsur Raji <monsurrajiybs@gmail.com>
2025-12-30 04:31:51 +0100

    ss

--- components/AboutSection/VisionMissionSection.tsx
+++ components/AboutSection/VisionMissionSection.tsx
@@ -4,11 +4,15 @@ import React from "react";
 export const VisionMissionSection: React.FC = () => {
   return (
     <section className="flex flex-col md:flex-row items-center justify-between gap-8 p-6 max-w-6xl mx-auto">
-      <img
-        src="https://api.builder.io/api/v1/image/assets/d246cf715b99493b8c80af048d853869/cbb048ecf88b04611cc398b219419e98065bf14c"
-        alt="Vision and Mission"
-        className="w-full md:w-1/2 rounded-lg object-cover"
-      />
+     <video
+  src="/about.mp4"
+  className="w-full md:w-1/2 rounded-lg object-cover"
+  autoPlay
+  muted
+  loop
+  playsInline
+/>
+
 
       <div className="md:w-1/2 space-y-6 text-black">
         <h2 className="text-3xl md:text-4xl font-bold">Our Vision and Mission</h2>
