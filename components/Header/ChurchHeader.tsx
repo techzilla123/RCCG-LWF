0572c1170fc7c939e620e0de0db4297897af8800
Monsur Raji <monsurrajiybs@gmail.com>
2026-01-09 04:47:39 +0100

    ss

--- components/Header/ChurchHeader.tsx
+++ components/Header/ChurchHeader.tsx
@@ -1,43 +1,48 @@
-"use client"
+  "use client"
 
-import type * as React from "react"
-import { useRouter, usePathname } from "next/navigation"
-import { AnnouncementBar } from "./AnnouncementBar"
-import { Logo } from "./Logo"
-import { Navigation } from "./Navigation"
-import { LiveSection } from "./LiveSection"
+  import type * as React from "react"
+  import { useRouter, usePathname } from "next/navigation"
+  import { AnnouncementBar } from "./AnnouncementBar"
+  import { Logo } from "./Logo"
+  import { Navigation } from "./Navigation"
+  import { LiveSection } from "./LiveSection"
 
-export const ChurchHeader: React.FC = () => {
-  const router = useRouter()
-  const pathname = usePathname()
+  export const ChurchHeader: React.FC = () => {
+    const router = useRouter()
+    const pathname = usePathname()
 
-  const handleLogoClick = () => {
-    if (pathname === "/") {
-      window.scrollTo({ top: 0, behavior: "smooth" })
-    } else {
-      router.push("/")
+    const handleLogoClick = () => {
+      if (pathname === "/") {
+        window.scrollTo({ top: 0, behavior: "smooth" })
+      } else {
+        router.push("/")
+      }
     }
+
+
+    return (
+      <header className="relative w-full bg-white h-[90px] md:h-[101px]">
+        <AnnouncementBar />
+
+<div className="absolute left-0 w-full h-[43px] top-[33px] z-[5] pointer-events-none pointer-events-none select-none">
+          <img
+            src="https://api.builder.io/api/v1/image/assets/TEMP/d6f10c255fb741589277e72738b85c3ad408fe64?width=2894"
+            alt=""
+            className="w-full rounded-3xl border-white border-solid backdrop-blur-[2.5px] border-[1.5px] h-[43px]"
+          />
+        </div>
+
+        {/* Logo now receives the click handler directly */}
+        <Logo onClick={handleLogoClick} />
+
+        <Navigation />
+<div className="relative z-[999] pointer-events-auto">
+  <LiveSection />
+</div>
+
+
+      </header>
+    )
   }
 
-  return (
-    <header className="relative w-full bg-white h-[90px] md:h-[101px]">
-      <AnnouncementBar />
-
-      <div className="absolute left-0 w-full h-[43px] top-[33px] z-[5]">
-        <img
-          src="https://api.builder.io/api/v1/image/assets/TEMP/d6f10c255fb741589277e72738b85c3ad408fe64?width=2894"
-          alt=""
-          className="w-full rounded-3xl border-white border-solid backdrop-blur-[2.5px] border-[1.5px] h-[43px]"
-        />
-      </div>
-
-      {/* Logo now receives the click handler directly */}
-      <Logo onClick={handleLogoClick} />
-
-      <Navigation />
-      <LiveSection />
-    </header>
-  )
-}
-
-export default ChurchHeader
+  export default ChurchHeader
